f7c58e99dd16554b05df78462c43290b0f30a34c
hth1000th <tahwang@puc.edu>
2016-04-25 03:06:55 -0700

    process scheduler file

---
+++ scheduler.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "queue.h"
+#include "scheduler.h"
+
+int stop() {
+}
+
+void ready(int pid) {
+}
+
+void sleep(int pid, int abs_time) {
+}
+
+void awaken(int pid) {
+}
+
+void block(int pid) {
+}
+
+int run_next() {
+}

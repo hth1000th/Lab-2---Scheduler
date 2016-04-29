cc9f900fbbbbbfdec887e58c6da6d579dd67fc47
hth1000th <tahwang@puc.edu>
2016-04-29 14:06:43 -0700

    process scheduler file(modified)

--- scheduler.c
+++ scheduler.c
@@ -2,13 +2,28 @@
 #include "queue.h"
 #include "scheduler.h"
 
-int stop() {
+int running(Queue *ready) {
+  int current_pid = run_next(ready);
+  return current_pid;
 }
 
-void ready(int pid) {
+int stop(int current_pid) {
+  if(current_pid != 0) {
+    return current_pid;
+  }
+  else {
+    return -1;
+  }
+}
+
+void ready(Queue *ready, int pid) {
+  enqueue(ready, pid);
 }
 
 void sleep(int pid, int abs_time) {
+  stop(pid);
+  enqueue(timed_wait, pid);
+
 }
 
 void awaken(int pid) {
@@ -17,5 +32,7 @@ void awaken(int pid) {
 void block(int pid) {
 }
 
-int run_next() {
+int run_next(Queue *ready) {
+  int next_pid = dequeue(ready);
+  return next_pid;
 }

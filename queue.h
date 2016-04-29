fbc6bdf1a1a9909f4c52f5fd6da823eeb5b25036
hth1000th <tahwang@puc.edu>
2016-04-29 14:06:25 -0700

    header file for queue.c(modified)

--- queue.h
+++ queue.h
@@ -1,7 +1,17 @@
-typedef struct queue *Queue;
-typedef struct process Process;
-Queue create();
-void delete(Queue qu);
-void enqueue(Queue qu, int pid);
-int dequeue(Queue qu);
-int peek(Queue qu);
+typedef struct process {
+  int pid;
+  int target_time;
+  struct process *next;
+} Process;
+
+typedef struct queue {
+  struct process *head;
+  struct process *tail;
+  int size;
+} Queue;
+
+Queue *create();
+void delete(Queue *qu);
+void enqueue(Queue *qu, int pid);
+int dequeue(Queue *qu);
+int peek(Queue *qu);

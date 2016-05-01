fc670a3a47d072bf7da45775b0e2afae845e8c55
hth1000th <tahwang@puc.edu>
2016-04-30 17:25:22 -0700

    process scheduler file(modified)

--- scheduler.c
+++ scheduler.c
@@ -2,37 +2,65 @@
 #include "queue.h"
 #include "scheduler.h"
 
-int running(Queue *ready) {
-  int current_pid = run_next(ready);
-  return current_pid;
-}
-
-int stop(int current_pid) {
-  if(current_pid != 0) {
-    return current_pid;
+int stop() {
+  int stopped_pid;
+  if(running_process > 0) {
+    stopped_pid = running_process;
+    running_process = 0;
+    return stopped_pid;
   }
   else {
-    return -1;
+    return 0;
   }
 }
 
-void ready(Queue *ready, int pid) {
-  enqueue(ready, pid);
+void ready(int pid) {
+  enqueue(ready_qu, pid);
 }
 
-void sleep(int pid, int abs_time) {
-  stop(pid);
-  enqueue(timed_wait, pid);
-
+void sleep(int abs_time) {
+  int pid = stop();
+  int target_time = global_clock + abs_time;
+  enqueue(timed_qu, pid);
+  timed_qu->tail->target_time = target_time;
+  if(!is_empty(ready_qu)) {
+    run_next();
+  }
 }
 
-void awaken(int pid) {
+void awaken() {
+  int pid = dequeue(timed_qu);
+  ready(pid);
 }
 
-void block(int pid) {
+void block() {
+  int i = 0;
+  int pid = stop();
+  while(io_list[i] > 0) {
+    i++;
+  }
+  io_list[i] = pid;
+  io_list[i+1] = 0;
+  if(!is_empty(ready_qu)) {
+    run_next();
+  }
 }
 
-int run_next(Queue *ready) {
-  int next_pid = dequeue(ready);
-  return next_pid;
+int run_next() {
+  running_process = dequeue(ready_qu);
+  return running_process;
+}
+
+void tick() {
+  global_clock++;
+  while(check_awaken()) {
+    awaken();
+    if(timed_qu->head == NULL) {
+      break;
+    }
+  }
+}
+
+int check_awaken() {
+  return global_clock == timed_qu->head->target_time;
 }

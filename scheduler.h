24882463345af3322db8f77d2557acfefbde331f
hth1000th <tahwang@puc.edu>
2016-04-30 17:26:01 -0700

    header file for scheduler.c(modified)

--- scheduler.h
+++ scheduler.h
@@ -1,11 +1,22 @@
-int running(Queue *ready);
-int stop(int current_pid); // stop the current running process if it exitsts, return its pid
-void ready(Queue *ready, int pid); // enqueue pid to ready queue;
+Queue *ready_qu;
+Queue *timed_qu;
+int io_list[50];
+int running_process;
+int global_clock;
+
+// stop the current running process if it exitsts, return its pid
+int stop();
+ // enqueue pid to ready queue;
+void ready(int pid);
 // stop(), calculate absolute time from relative time, add pid to wait list,
 // run_next()
-void sleep(int pid, int abs_time);
-void awaken(int pid); // remove from wait list, ready;
-void block(int pid); // stop(), add pid to I/O list, run_next();
+void sleep(int abs_time);
+// remove from wait list, ready;
+void awaken();
+void block();
+// stop(), add pid to I/O list, run_next();
 // dequeue the next process from the ready queue, ready()
 // previous process if it exists;
 int run_next();
+void tick();
+int check_awaken() ;
